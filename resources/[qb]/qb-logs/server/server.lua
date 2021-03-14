461687d05d2a96be9c884baf01c922ea0b3cba55
JamesUK2020 <60899490+JamesUK2020@users.noreply.github.com>
2021-03-14 18:53:17 +0000

    add: fix to users using logs

--- resources/[qb]/qb-logs/server/server.lua
+++ resources/[qb]/qb-logs/server/server.lua
@@ -4,23 +4,25 @@ local loggingApi = "https://qbus.onno204.nl/qbus-management/backend/fivem/log"
 
 RegisterServerEvent('qb-log:server:CreateLog')
 AddEventHandler('qb-log:server:CreateLog', function(name, title, color, message, tagEveryone)
-    local tag = tagEveryone ~= nil and tagEveryone or false
-    local webHook = Config.Webhooks[name] ~= nil and Config.Webhooks[name] or Config.Webhooks["default"]
-    local embedData = {
-        {
-            ["title"] = title,
-            ["color"] = Config.Colors[color] ~= nil and Config.Colors[color] or Config.Colors["default"],
-            ["footer"] = {
-                ["text"] = os.date("%c"),
-            },
-            ["description"] = message,
+    if source ~= 0 then  -- Prevents everyone abusing logs.
+        local tag = tagEveryone ~= nil and tagEveryone or false
+        local webHook = Config.Webhooks[name] ~= nil and Config.Webhooks[name] or Config.Webhooks["default"]
+        local embedData = {
+            {
+                ["title"] = title,
+                ["color"] = Config.Colors[color] ~= nil and Config.Colors[color] or Config.Colors["default"],
+                ["footer"] = {
+                    ["text"] = os.date("%c"),
+                },
+                ["description"] = message,
+            }
         }
-    }
-    PerformHttpRequest(webHook, function(err, text, headers) end, 'POST', json.encode({ username = "Criminal Logs",embeds = embedData}), { ['Content-Type'] = 'application/json' })
-    Citizen.Wait(100)
-    if tag then
-        PerformHttpRequest(webHook, function(err, text, headers) end, 'POST', json.encode({ username = "Criminal Logs", content = "@everyone"}), { ['Content-Type'] = 'application/json' })
-    end
+        PerformHttpRequest(webHook, function(err, text, headers) end, 'POST', json.encode({ username = "Criminal Logs",embeds = embedData}), { ['Content-Type'] = 'application/json' })
+        Citizen.Wait(100)
+        if tag then
+            PerformHttpRequest(webHook, function(err, text, headers) end, 'POST', json.encode({ username = "Criminal Logs", content = "@everyone"}), { ['Content-Type'] = 'application/json' })
+        end
+end
 end)
 
 RegisterServerEvent('qb-log:server:sendLog')
--- resources/[standalone]/hospital/client/dead.lua
+++ resources/[standalone]/hospital/client/dead.lua
@@ -27,7 +27,7 @@ Citizen.CreateThread(function()
                 local killerName = killerId ~= -1 and GetPlayerName(killerId) .. " " .. "("..GetPlayerServerId(killerId)..")" or "Ele proprio ou NPC"
                 local weaponLabel = QBCore.Shared.Weapons[killerWeapon] ~= nil and QBCore.Shared.Weapons[killerWeapon]["label"] or "Desconhecido"
                 local weaponName = QBCore.Shared.Weapons[killerWeapon] ~= nil and QBCore.Shared.Weapons[killerWeapon]["name"] or "Arma_Desconhecida"
-                TriggerServerEvent("qb-log:server:CreateLog", "death", GetPlayerName(player) .. " ("..GetPlayerServerId(player)..") morreu", "red", "**".. killerName .. "** matou ".. GetPlayerName(player) .." com **".. weaponLabel .. "** (" .. weaponName .. ")")
+                --TriggerServerEvent("qb-log:server:CreateLog", "death", GetPlayerName(player) .. " ("..GetPlayerServerId(player)..") morreu", "red", "**".. killerName .. "** matou ".. GetPlayerName(player) .." com **".. weaponLabel .. "** (" .. weaponName .. ")")
                 deathTime = Config.DeathTime
                 OnDeath()
                 DeathTimer()
